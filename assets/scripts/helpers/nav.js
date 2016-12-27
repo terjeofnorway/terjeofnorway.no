68d1c4b795186593b3e4013ec448462933f15a78
terjeofnorway <terjeofnorway@gmail.com>
2016-12-27 21:17:29 +0100

    Adjusted animation for dropdown menu

--- assets/scripts/helpers/nav.js
+++ assets/scripts/helpers/nav.js
@@ -6,14 +6,19 @@ class Nav {
     }
 
 
+    /** Add event listeners for mist aspects of the Nav item.
+     * @return void
+     */
     addEventListeners(){
         document.getElementById('mobileMenuToggle').addEventListener('click', this.mobileMenuClickEventListener.bind(this));
     }
 
+
+    /** Expand or collapse the mobile menu when the menu item is toggled.
+     * @return void
+      */
     mobileMenuClickEventListener(){
         let menu = document.getElementById('navigation').getElementByName('ul');
-
         menu.hasClass('collapsed') == true ? menu.removeClass('collapsed') : menu.addClass('collapsed');
-
     }
 }
